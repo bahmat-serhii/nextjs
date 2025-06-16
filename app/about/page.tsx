261f330fc3212e847d0746d7d90157e7294540b2
Bakhmatov_Serhii <bakhmatov1982@gmail.com>
2025-06-16 17:29:53 +0300

    lessons 1

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+// app/about/page.tsx
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About page",
+};
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
---
+++ app/notes/page.tsx
@@ -0,0 +1,12 @@
+// app/notes/page.tsx
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Notes page",
+};
+
+const Notes = () => {
+  return <div>Notes</div>;
+};
+
+export default Notes;
